1e8a4a9d96914315defc31cce2e9a739ddcf7fd4
cyclic-reference <alex@cyclicreference.space>
2017-12-09 09:33:07 -0600

    less secure

--- web-service/src/main/java/io/acari/landing/auth/SecurityUtils.java
+++
@@ -1,7 +0,0 @@
-package io.acari.landing.auth;
-
-public class SecurityUtils {
-  public static final long EXPIRATION_TIME = 864_000_000; // 10 days
-  public static final String TOKEN_PREFIX = "Bearer ";
-  public static final String HEADER_STRING = "Authorization";
-}
