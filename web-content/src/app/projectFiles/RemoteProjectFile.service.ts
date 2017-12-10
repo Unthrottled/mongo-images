8cc94b5da9a6ea01dabbbae70e08b34a594499a1
cyclic-reference <alex@cyclicreference.space>
2017-12-10 12:03:53 -0600

    remote project service

---
+++ web-content/src/app/projectFiles/RemoteProjectFile.service.ts
@@ -0,0 +1,10 @@
+
+import {Injectable} from "@angular/core";
+import {BackendAPIService} from "../BackendAPI.service";
+
+@Injectable()
+export class RemoteProjectFileService {
+
+    constructor(private backendAPISevice: BackendAPIService){}
+
+}
