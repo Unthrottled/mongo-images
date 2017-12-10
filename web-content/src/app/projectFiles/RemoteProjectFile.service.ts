97834d3528e45829d67f07ac16a70531c4044a44
cyclic-reference <alex@cyclicreference.space>
2017-12-10 12:05:36 -0600

    remote project service guts

--- web-content/src/app/projectFiles/RemoteProjectFile.service.ts
+++ web-content/src/app/projectFiles/RemoteProjectFile.service.ts
@@ -1,10 +1,27 @@
 
 import {Injectable} from "@angular/core";
 import {BackendAPIService} from "../BackendAPI.service";
+import {WindowRef} from "../window";
+import {RemoteProjectFile} from "./RemoteProjectFile";
+import {Identifier} from "./Identifier.model";
 
 @Injectable()
 export class RemoteProjectFileService {
 
-    constructor(private backendAPISevice: BackendAPIService){}
+    constructor(private backendAPISevice: BackendAPIService, private windowRef: WindowRef){}
+
+    public fetchRemoteProject(fileId: string): RemoteProjectFile {
+        return new RemoteProjectFile(new Identifier(fileId),
+            this.backendAPISevice.fetchImage(fileId)
+                .map(arrayBuffer=>{
+                    let binary = '';
+                    let bytes = new Uint8Array(arrayBuffer);
+                    let len = bytes.byteLength;
+                    for(let i = 0; i < len; ++i){
+                        binary += String.fromCharCode(bytes[i]);
+                    }
+                    return 'data:image/png;base64,' + this.windowRef.nativeWindow.btoa(binary);
+                }));
+    }
 
 }
