9c8d2c18dd4e4d8b71c50cafb8b2097909c32c75
cyclic-reference <alex@cyclicreference.space>
2017-12-09 21:52:35 -0600

    component for project files

---
+++ web-content/src/app/projectFiles/ProjectFileList.component.ts
@@ -0,0 +1,21 @@
+
+
+import {Component, Input} from "@angular/core";
+import {ProjectFile} from "./ProjectFile.model";
+
+@Component({
+    selector: 'project-file-list',
+    template: require('./ProjectFileList.component.htm')
+})
+export class ProjectFileListComponent {
+    private _projectFiles: ProjectFile[] = [];
+
+    @Input()
+    get projectFiles(): ProjectFile[] {
+        return this._projectFiles;
+    }
+
+    set projectFiles(value: ProjectFile[]) {
+        this._projectFiles = value;
+    }
+}
