b0dc6f07f85adea7ffa59ad2424ede4e97771bd5
cyclic-reference <alex@cyclicreference.space>
2017-12-10 12:18:19 -0600

    project file component

--- web-content/src/app/projectFiles/ProjectFile.module.ts
+++ web-content/src/app/projectFiles/ProjectFile.module.ts
@@ -5,6 +5,7 @@ import {RemoteProjectFileService} from "./RemoteProjectFile.service";
 import {ProjectFileChooseComponent} from "./ProjectFileChoose.component";
 import {ProjectFileListComponent} from "./ProjectFileList.component";
 import {ProjectFileViewComponent} from "./ProjectFileView.component";
+import {ProjectFileComponent} from "./ProjectFile.component";
 
 @NgModule({
         imports: [],
@@ -12,7 +13,8 @@ import {ProjectFileViewComponent} from "./ProjectFileView.component";
         declarations:[
             ProjectFileChooseComponent,
             ProjectFileListComponent,
-            ProjectFileViewComponent
+            ProjectFileViewComponent,
+            ProjectFileComponent
         ],
         bootstrap: [],
         providers: [RemoteProjectFileService],
