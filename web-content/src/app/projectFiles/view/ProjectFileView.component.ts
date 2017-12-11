7c8276eaf32eaea68512df6240b50626fdfee8c2
cyclic-reference <alex@cyclicreference.space>
2017-12-11 11:42:46 -0600

    change places

--- web-content/src/app/projectFiles/view/ProjectFileView.component.ts
+++ web-content/src/app/projectFiles/view/ProjectFileView.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {Observable} from "rxjs/Observable";
 import {ProjectFile} from "../model/ProjectFile.model";
 
