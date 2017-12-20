278d76dcb6c03bc1aae2dd32550dc6bf695842ca
DATBOI <alex@cyclicreference.space>
2017-12-20 13:49:22 -0600

    this?

--- web-service/src/main/java/io/acari/images/FluxAsyncInputStream.java
+++ web-service/src/main/java/io/acari/images/FluxAsyncInputStream.java
@@ -5,25 +5,47 @@ import com.mongodb.reactivestreams.client.gridfs.AsyncInputStream;
 import org.reactivestreams.Publisher;
 import org.springframework.core.io.buffer.DataBuffer;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 
 import java.nio.ByteBuffer;
 
+/**
+ * The Async Input Stream interface represents some asynchronous input stream of bytes.
+ *
+ */
 public class FluxAsyncInputStream implements AsyncInputStream {
 
   private final Flux<DataBuffer> source;
 
   public FluxAsyncInputStream(Flux<DataBuffer> source) {
     this.source = source;
+
   }
 
+  /**
+   * Reads a sequence of bytes from this stream into the given buffer.
+   *
+   * @param dst      the destination buffer
+   * @return a publisher with a single element, the total number of bytes read into the buffer, or
+   *         {@code -1} if there is no more data because the end of the stream has been reached.
+   */
   @Override
   public Publisher<Integer> read(ByteBuffer dst) {
-    return null;
+    return this.source.next()
+        .map(dataBuffer -> {
+          dst.put(dataBuffer.asByteBuffer());
+          return dataBuffer.readableByteCount() <= 0 ? -1 : dataBuffer.readableByteCount();
+        }).defaultIfEmpty(-1);
   }
 
+  /**
+   * Closes the input stream
+   *
+   * @return a publisher with a single element indicating when the stream has been closed
+   */
   @Override
   public Publisher<Success> close() {
-    return null;
+    return Mono.just(Success.SUCCESS);//The stream was never really open.
   }
 }
